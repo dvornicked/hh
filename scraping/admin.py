2442bdbc6dc347bd1a863c4e9d96e83faeba8dc4
dvornick <dev@dvornick.ru>
2022-10-27 03:11:22 +0300

    Create model Language

--- scraping/admin.py
+++ scraping/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import City
+from .models import City, Language
 
 
 @admin.register(City)
 class CityAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
     pass
--- scraping/models.py
+++ scraping/models.py
@@ -10,3 +10,11 @@ class City(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    slug = models.CharField(max_length=32, blank=True, unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
