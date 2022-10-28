a575100f1babd1f0d87eabf8ed10125bd7347941
dvornick <mail@dvornick.ru>
2022-10-28 05:58:50 +0300

    Add hh_id field to City model

--- scraping/models.py
+++ scraping/models.py
@@ -6,6 +6,8 @@ class City(models.Model):
     name = models.CharField(max_length=32, unique=True)
     slug = models.SlugField(max_length=32, unique=True)
 
+    hh_id = models.IntegerField(unique=True, null=True)
+
     class Meta:
         verbose_name_plural = 'Cities'
 
