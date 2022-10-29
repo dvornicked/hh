78dc7a641a682964d8abd184863ba15e9bfd1aed
dvornick <dev@dvornick.ru>
2022-10-30 02:53:11 +0300

    Refactor scraper and notify scripts

--- account/notify.py
+++ account/notify.py
@@ -9,26 +9,27 @@ django.setup()
 from scraping.models import Vacancy
 from account.models import Profile
 
-qs = Profile.objects.filter(notify=True).select_related('user').values('user__email', 'city', 'language')
-users_dct = {}
-vacancies_dct = {}
-for user in qs: users_dct.setdefault((user['city'], user['language']), []).append(user['user__email'])
-params = {'city_id__in': [key[0] for key in users_dct.keys()],
-            'language_id__in': [key[1] for key in users_dct.keys()]}
-qs = Vacancy.objects.filter(**params).values().order_by('timestamp')
-vacancies_dct = {(vacancy['city_id'], vacancy['language_id']): [] for vacancy in qs}
-for vacancy in qs: vacancies_dct[(vacancy['city_id'], vacancy['language_id'])].append(vacancy)
 
-for keys, emails in users_dct.items():
-    vacancies = vacancies_dct.get(keys, [])
-    if vacancies:
-        subject = f'Found new vacancies'
-        text_content = f'Found new vacancies'
-        html_content = '<h2>Found new vacancies:</h2>'
-        for vacancy in vacancies[:4]:
-            html_content += f'<a href="{vacancy["url"]}">{vacancy["title"]}</a><br>'
-            html_content += f'<p>{vacancy["description"]}</p><br><hr>'
-        for email in emails:
-            msg = EmailMultiAlternatives(subject, text_content, config('EMAIL_HOST_USER'), [email])
-            msg.attach_alternative(html_content, 'text/html')
-            msg.send()
+def run():
+    qs = Profile.objects.filter(notify=True).select_related('user').values('user__email', 'city', 'language')
+    users_dct = {}
+    for user in qs: users_dct.setdefault((user['city'], user['language']), []).append(user['user__email'])
+    params = {'city_id__in': [key[0] for key in users_dct.keys()],
+              'language_id__in': [key[1] for key in users_dct.keys()]}
+    qs = Vacancy.objects.filter(**params).values().order_by('timestamp')
+    vacancies_dct = {(vacancy['city_id'], vacancy['language_id']): [] for vacancy in qs}
+    for vacancy in qs: vacancies_dct[(vacancy['city_id'], vacancy['language_id'])].append(vacancy)
+
+    for keys, emails in users_dct.items():
+        vacancies = vacancies_dct.get(keys, [])
+        if vacancies:
+            subject = f'Found new vacancies'
+            text_content = f'Found new vacancies'
+            html_content = '<h2>Found new vacancies:</h2>'
+            for vacancy in vacancies[:4]:
+                html_content += f'<a href="{vacancy["url"]}">{vacancy["title"]}</a><br>'
+                html_content += f'<p>{vacancy["description"]}</p><br><hr>'
+            for email in emails:
+                msg = EmailMultiAlternatives(subject, text_content, config('EMAIL_HOST_USER'), [email])
+                msg.attach_alternative(html_content, 'text/html')
+                msg.send()
--- scraping/scraper.py
+++ scraping/scraper.py
@@ -9,55 +9,53 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hh.settings')
 django.setup()
 
 from scraping.models import Vacancy, City, Language
-from account.models import Profile
-
-headers = {'User-Agent': '"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                         'Chrome/74.0.3729.169 Safari/537.36"'}
-
-vacancies = []
 
 
-def hh(city, language):
-    url = f'https://hh.ru/search/vacancy?area={city.hh_id}&text={language.name}'
-    response = requests.get(url=url, headers=headers)
-    results = []
-    if response.status_code == 200:
-        soup = BSoup(response.content, 'html.parser')
-        container = soup.find('div', attrs={'data-qa': 'vacancy-serp__results'})
-        vacancy_nodes = container.find_all('div', attrs={'class': 'serp-item'})
-        for node in vacancy_nodes:
-            title_node = node.find('a')
-            title = title_node.text
-            url = title_node['href']
-            company = node.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
-            description = (node.find('div',
-                                     attrs={'data-qa': 'vacancy-serp__vacancy_snippet_responsibility'}) or node.find(
-                'div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_requirement'})).text
-            results.append(
-                {'title': title, 'url': url, 'company': company, 'description': description, 'language': language,
-                 'city': city})
-    return results
+def run():
+    headers = {'User-Agent': '"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/74.0.3729.169 Safari/537.36"'}
 
+    vacancies = []
 
-def get_list():
-    cities = City.objects.all()
-    languages = Language.objects.all()
-    return [(city, language) for city in cities for language in languages]
+    def hh(city, language):
+        url = f'https://hh.ru/search/vacancy?area={city.hh_id}&text={language.name}'
+        response = requests.get(url=url, headers=headers)
+        results = []
+        if response.status_code == 200:
+            soup = BSoup(response.content, 'html.parser')
+            container = soup.find('div', attrs={'data-qa': 'vacancy-serp__results'})
+            vacancy_nodes = container.find_all('div', attrs={'class': 'serp-item'})
+            for node in vacancy_nodes:
+                title_node = node.find('a')
+                title = title_node.text
+                url = title_node['href']
+                company = node.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
+                description = (node.find('div',
+                                         attrs={
+                                             'data-qa': 'vacancy-serp__vacancy_snippet_responsibility'}) or node.find(
+                    'div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_requirement'})).text
+                results.append(
+                    {'title': title, 'url': url, 'company': company, 'description': description, 'language': language,
+                     'city': city})
+        return results
 
+    def get_parse_list():
+        cities = City.objects.all()
+        languages = Language.objects.all()
+        return [(city, language) for city in cities for language in languages]
 
-async def main(value):
-    results = await loop.run_in_executor(None, hh, *value)
-    vacancies.extend(results)
+    async def main(value):
+        results = await loop.run_in_executor(None, hh, *value)
+        vacancies.extend(results)
 
-
-parse_list = get_list()
-if parse_list:
-    loop = asyncio.get_event_loop()
-    tasks = asyncio.wait([loop.create_task(main(value)) for value in parse_list()])
-    loop.run_until_complete(tasks)
-    loop.close()
-    for vacancy in vacancies:
-        try:
-            Vacancy.objects.create(**vacancy)
-        except Exception as e:
-            print(e)
+    parse_list = get_parse_list()
+    if parse_list:
+        loop = asyncio.get_event_loop()
+        tasks = asyncio.wait([loop.create_task(main(value)) for value in parse_list])
+        loop.run_until_complete(tasks)
+        loop.close()
+        for vacancy in vacancies:
+            try:
+                Vacancy.objects.create(**vacancy)
+            except Exception as e:
+                print(e)
